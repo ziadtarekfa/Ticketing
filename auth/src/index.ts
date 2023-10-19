3298cf52ce3f2070ae412fd8bf363476b53e62a1
Ziad Tarek <ziadtarekfa@gmail.com>
2023-10-19 22:32:51 +0300

    set up ingress nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get('/api/users/currentUser', (req, res) => {
+    res.send("Hi there !");
+})
+
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 })
