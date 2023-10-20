788eb538404d47ab72726cd0cfc4c88e3e02e1c3
Ziad Tarek <ziadtarekfa@gmail.com>
2023-10-21 02:13:52 +0300

    validation email and password : auth

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,26 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.get('/api/users/signup', (req, res) => {
-    
-});
+router.get('/api/users/signup', [
+    body("email").isEmail().withMessage("Email must be valid !"),
+    body("password").trim().isLength({ min: 4, max: 20 })
+        .withMessage("Password must be between 4 and 20 characters")
+],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).send(errors.array());
+        }
+        else {
+            const { email, password } = req.body;
+            console.log("Creating user");
+            
+            res.send({});
+        }
+
+
+    });
 
 export { router as signUpRouter };
