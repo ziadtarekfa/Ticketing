22bee579b3bb71be23011fe5f1b9d7ca8f074df9
Ziad Tarek <ziadtarekfa@gmail.com>
2023-10-21 20:29:11 +0300

    set up mongoose : auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { errorHandler } from "./middlewares/error-handling";
 import { json } from "body-parser";
-
+import mongoose from "mongoose";
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
@@ -23,8 +23,18 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log("Connected to MongoDb");
+    } catch (err) {
+        console.error(err);
+    }
 
+    app.listen(3000, () => {
+        console.log("Listening on port 3000");
+    });
+}
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
-});
+start();
+
