613220c88b92c37f89fc9d29c904b7fa5db8629e
Ziad Tarek <ziadtarekfa@gmail.com>
2023-10-26 22:42:20 +0300

    implemented unit tests for routes : auth

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
+import request from "supertest";
+import app from "../app";
 import { MongoMemoryServer } from "mongodb-memory-server";
+
+declare global {
+    var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -22,3 +28,20 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password
+        })
+        .expect(200);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+};
