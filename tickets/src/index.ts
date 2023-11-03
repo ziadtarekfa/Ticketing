1de2320cee32c2a649070d50477b7bf511e23f37
Ziad Tarek <ziadtarekfa@gmail.com>
2023-11-04 01:05:20 +0200

    set up mongoose : tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import app from "./app";
 const start = async () => {
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        await mongoose.connect(process.env.MONGO_URI || '');
         console.log("Connected to MongoDb");
     } catch (err) {
         console.error(err);
