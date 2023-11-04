7676aa12fe095ee4d46021202071f440c77402da
Ziad Tarek <ziadtarekfa@gmail.com>
2023-11-04 17:17:55 +0200

    implemented get tickets : tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,13 +4,18 @@ import { NotFoundError } from "@ziadtarekfatickets/common";
 import { json } from "body-parser";
 import cookieParser from 'cookie-parser';
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 
 const app = express();
 app.use(json());
 app.use(cookieParser());
 app.use(currentUser);
+
+app.use(showTicketRouter)
 app.use(createTicketRouter);
+app.use(indexTicketRouter);
 
 
 
---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import app from "../../app";
+import mongoose from "mongoose";
+
+it('returns a 404 if the ticket is not found', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app)
+        .get(`/api/tickets/${id}`)
+        .send()
+        .expect(404);
+});
+
+
+it('returns the ticket if the ticket is found', async () => {
+    const cookie = await global.signin();
+    const title = 'concert';
+    const price = 20;
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title,
+            price
+        })
+        .expect(201);
+
+
+    console.log(response.body);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body._id}`)
+        .send();
+
+    console.log(ticketResponse);
+
+
+    expect(ticketResponse.body.title).toEqual(title);
+    expect(ticketResponse.body.price).toEqual(price);
+
+});
