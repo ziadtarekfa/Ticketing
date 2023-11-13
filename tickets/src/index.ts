389abf957f82ba70f7e7ceaa2a04fb6b575fe7a0
Ziad Tarek <ziadtarekfa@gmail.com>
2023-11-14 00:05:09 +0200

    handled emmiting event for ticket creation : tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,10 +1,22 @@
 import mongoose from "mongoose";
 import app from "./app";
+import { natsWrapper } from "./nats-wrapper";
 const start = async () => {
     try {
+        await natsWrapper.connect('ticketing', 'vdvdf', 'http://nats-srv:4222');
+
+        natsWrapper.client.on('close', () => {
+            console.log('Connection is closed');
+            process.exit();
+        });
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
         await mongoose.connect(process.env.MONGO_URI || '');
+
         console.log("Connected to MongoDb");
     } catch (err) {
+        console.log("FUCK");
+
         console.error(err);
     }
 
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@ziadtarekfatickets/common';
 import { buildTicket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -23,6 +25,14 @@ router.post(
         const ticket = buildTicket(title, price, req.currentUser!.id);
 
         await ticket.save();
+
+        new TicketCreatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId
+        });
+
         res.status(201).send(ticket);
     }
 );
