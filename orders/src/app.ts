900ff04280fb183fd467127a8c4586697a905e1a
Ziad Tarek <ziadtarekfa@gmail.com>
2023-11-14 19:16:56 +0200

    set up orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { currentUser, errorHandler } from "@ziadtarekfatickets/common";
+import { NotFoundError } from "@ziadtarekfatickets/common";
+import { json } from "body-parser";
+import cookieParser from 'cookie-parser';
+
+// import { showTicketRouter } from "./routes/show";
+// import { indexTicketRouter } from "./routes";
+// import { createTicketRouter } from "./routes/new";
+// import { updateTicketRouter } from "./routes/update";
+
+
+
+const app = express();
+app.use(json());
+app.use(cookieParser());
+app.use(currentUser);
+
+
+// app.use(createTicketRouter);
+// app.use(showTicketRouter)
+// app.use(indexTicketRouter);
+// app.use(updateTicketRouter);
+
+
+app.all('*', (_req, _res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+
+export default app;
