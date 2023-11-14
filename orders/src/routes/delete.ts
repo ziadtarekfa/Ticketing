d29a087ecf7be31936a492a2dbb076575304592f
Ziad Tarek <ziadtarekfa@gmail.com>
2023-11-15 00:58:05 +0200

    implemented cancel order : orders

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,12 +1,25 @@
 import express, { Request, Response } from 'express';
-// import { Order } from '../models/order';
+import { Order } from '../models/order';
+import { NotAuthorizedError, NotFoundError, OrderStatus, requireAuth } from '@ziadtarekfatickets/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-    // const tickets = await Ticket.find({});
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
 
-    res.status(200).send({});
+    if (!order) {
+        throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser.id) {
+        throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // publish an event for cancellation
+
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
