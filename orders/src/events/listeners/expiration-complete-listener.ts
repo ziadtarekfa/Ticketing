e585ec0b070150668e7ee96f43d4dab2447f195c
Ziad Tarek <ziadtarekfa@gmail.com>
2023-11-20 14:52:02 +0200

    expirtaion-complete-listener : orders

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,34 @@
+import { ExpirationCompleteEvent, Listener, OrderStatus, Subjects, TicketCreatedEvent } from "@ziadtarekfatickets/common";
+import { Message } from "node-nats-streaming";
+import { buildTicket } from "../../models/ticket";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+import { natsWrapper } from "../../nats-wrapper";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent>{
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+    queueGroupName = 'orders-service';
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const order = await Order.findById(data.orderId).populate('ticket');
+        if (!order) {
+            throw new Error('Order Not Found');
+        }
+
+        order.set({
+            status: OrderStatus.Cancelled
+        });
+
+        await order.save();
+
+        new OrderCancelledPublisher(natsWrapper.client).publish({
+            id: order._id,
+            version: order.version,
+            ticket: {
+                id: order.ticket._id
+            }
+        });
+
+        msg.ack();
+    }
+}
