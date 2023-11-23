b5993a84c21094949dc5232b6a983d5e89d7e553
Ziad Tarek <ziadtarekfa@gmail.com>
2023-11-23 21:44:39 +0200

    client finished

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,30 +2,34 @@ import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
 import Header from '../components/header';
 
-const AppComponent = ({ Component, pageProps,currentUser }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
-
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <div className="container">
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
   );
 };
 
-AppComponent.getInitialProps = async appContext => {
+AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get('/api/users/currentuser');
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
   }
 
   return {
     pageProps,
-    ...data
+    ...data,
   };
 };
-
 
 export default AppComponent;
--- client/pages/auth/signout.js
+++ client/pages/auth/signout.js
@@ -3,16 +3,16 @@ import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 export default () => {
-    const { doRequest } = useRequest({
-        url: '/api/users/signout',
-        method: 'post',
-        body: {},
-        onSuccess: () => Router.push('/')
-    });
+  const { doRequest } = useRequest({
+    url: '/api/users/signout',
+    method: 'post',
+    body: {},
+    onSuccess: () => Router.push('/')
+  });
 
-    useEffect(() => {
-        doRequest();
-    }, []);
+  useEffect(() => {
+    doRequest();
+  }, []);
 
-    return <div>Signing you out...</div>;
+  return <div>Signing you out...</div>;
 };
